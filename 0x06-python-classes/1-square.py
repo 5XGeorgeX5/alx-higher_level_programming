64dcaa33715f9de9edbd290d01d89f0cef2290b5
George Raafat <georgexraafat@gmail.com>
2023-06-27 09:34:02 -0700

    a

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Define a class square.
+"""
+
+
+class Square:
+    """
+    A class square the defines a square.
+    """
+
+    def __init__(self, size):
+        """
+        initialize a new Square.
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
