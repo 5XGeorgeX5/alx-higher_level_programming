efc94c3226fce772c37622fed822b3e6a5289ddb
George Raafat <georgexraafat@gmail.com>
2023-06-27 09:28:01 -0700

    a

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""A module for square"""
+"""
+Define a class square
+"""
 
 
 class Square:
-    """Represent a square."""
+    """
+    An empty class square the defines a square
+    """
     pass
-
