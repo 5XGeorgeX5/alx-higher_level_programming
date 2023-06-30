215ea898d1087c84554de74ef37e8c4bc9499ee5
George Raafat <georgexraafat@gmail.com>
2023-06-30 10:24:05 -0700

    a

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a print_square function."""
+
+
+def print_square(size):
+    """
+    Print a square.
+    Args:
+        first_name (int): The square size.
+    Raises:
+        TypeError: If size is not int.
+        ValueError: If size is less than 0.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print('#' * size)
