f04baf766c54eaf3a00e1dab8183cc817e7958a3
George Raafat <georgexraafat@gmail.com>
2023-09-14 14:21:13 -0700

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# lists all states from the database hbtn_0e_0_usa.
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = database.cursor()
+    query = """
+SELECT *
+FROM states
+WHERE BINARY name LIKE 'N%'
+ORDER BY id
+"""
+    cursor.execute(query)
+    for state in cursor.fetchall():
+        print(state)
