1f113b90397c4b6b2ba370f1d5396bcb8f5bd314
George Raafat <georgexraafat@gmail.com>
2023-09-14 14:53:43 -0700

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# lists all states from the database hbtn_0e_0_usa.
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = database.cursor()
+    query = """
+SELECT
+    c.id,
+    c.name,
+    s.name
+FROM cities c
+JOIN states s
+    ON c.state_id = s.id
+ORDER BY c.id
+"""
+    cursor.execute(query)
+    for city in cursor.fetchall():
+        print(city)
