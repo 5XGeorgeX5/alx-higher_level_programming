09069ee30e90a0060a6453fdaf7cbd3a8998d29b
George Raafat <georgexraafat@gmail.com>
2023-09-15 08:45:34 -0700

    doing some task

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""deletes all State objects with a name containing the letter a from hbtn_0e_6_usa"""
+"""deletes all States that contain the letter a from hbtn_0e_6_usa"""
 from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
