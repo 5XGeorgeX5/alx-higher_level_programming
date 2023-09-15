bc194bf7f32e8c8c4b0d17ba0f4d115d889f14ba
George Raafat <georgexraafat@gmail.com>
2023-09-15 07:50:01 -0700

    doing some task

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""lists all State objects from hbtn_0e_6_usa"""
+"""lists all State objects that contain the letter a from hbtn_0e_6_usa"""
 from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
