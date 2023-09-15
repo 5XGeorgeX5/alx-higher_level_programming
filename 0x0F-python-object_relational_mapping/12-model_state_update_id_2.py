a9bef1a6baaf3c94b0dca77bf85bafaca2f60cc3
George Raafat <georgexraafat@gmail.com>
2023-09-15 08:37:50 -0700

    task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""prints the id of a passed State name"""
+"""changes the name of a State object from hbtn_0e_6_usa"""
 from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
@@ -10,9 +10,8 @@ if __name__ == '__main__':
         @localhost/{argv[3]}', pool_pre_ping=True)
     start_Session = sessionmaker(bind=engine)
     session = start_Session()
-    state = session.query(State).filter_by(name=argv[4]).first()
+    state = session.query(State).filter_by(id=2).first()
     if state:
-        print(state.id)
-    else:
-        print('Not found')
+        state.name = "New Mexico"
+        session.commit()
     session.close()
