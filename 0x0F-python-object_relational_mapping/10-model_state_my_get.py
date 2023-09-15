a6f5412ee36888c267f4b4ae9c1d61eb73d602a7
George Raafat <georgexraafat@gmail.com>
2023-09-15 08:14:07 -0700

    task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""lists all State objects that contain the letter a from hbtn_0e_6_usa"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]} \
+        @localhost/{argv[3]}', pool_pre_ping=True)
+    start_Session = sessionmaker(bind=engine)
+    session = start_Session()
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state:
+        print(state.id)
+    else:
+        print('Not found')
+    session.close()
