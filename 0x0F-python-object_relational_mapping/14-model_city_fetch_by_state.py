5af18c4d4fd98a021f98fc878f48e6d245642831
George Raafat <georgexraafat@gmail.com>
2023-09-15 09:16:47 -0700

    doing some task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""deletes all States that contain the letter a from hbtn_0e_6_usa"""
+"""prints all Cities with their states from hbtn_0e_6_usa"""
 from sys import argv
 from model_state import Base, State
 from model_city import City
