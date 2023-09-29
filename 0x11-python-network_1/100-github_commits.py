4484edf204ce477de40afeb2393d88a1d90bd13b
George Raafat <georgexraafat@gmail.com>
2023-09-29 07:32:42 -0700

    task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""takes yourusername and repo on GitHub and displays last 10 commits"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from requests import get
+
+    response = get("https://api.github.com/repos/" \
+                   f"{argv[1]}/{argv[2]}/commits?per_page=10")
+    commits = response.json()
+    for commit in commits:
+        name = commit.get('commit').get('author').get('name')
+        print(f"{commit.get('sha')}: {name}")
